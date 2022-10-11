4ed29aea29878572a857b424b4add1ddb400e9a0
Justinas Pekarskis <Justinas.Pekarskis@euromonitor.com>
2022-10-11 14:47:49 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,59 @@
+import csv
+from datetime import datetime
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+from bs4 import BeautifulSoup
+import os
+import time
+
+
+currentTime = datetime.now()
+ser = Service(os.path.abspath("assets/chromedriver.exe"))
+options = Options()
+#options.headless = True
+options.add_argument("--window-size=640,480")
+driver = webdriver.Chrome(service=ser, options=options,)
+
+item_list = []
+
+def getPageData(url):
+    driver.get(url)
+    #print(driver.page_source)
+    all_items = driver.find_elements(By.CLASS_NAME, 'list-row')
+    for item in all_items:
+        item_html = BeautifulSoup(item.get_attribute('outerHTML'), "html.parser")
+        if item_html.find('span', {"class": "list-item-price"}):
+            item_dictionary = {}
+            item_dictionary['name'] = item_html.find('div', {"class":"list-photo"}).findNext('a').findNext('img')['alt']
+            item_dictionary["image"] = item_html.find('div', {"class":"list-photo"}).findNext('a').findNext('img')['src']
+            item_dictionary["url"] = item_html.find('div', {"class":"list-photo"}).findNext('a')['href']
+            item_dictionary["price"] = item_html.find('span', {"class": "list-item-price"}).text.replace('€', '').strip()
+            item_dictionary["price_for_sqm"] = item_html.find('span', {"class": "price-pm"}).text.replace('€/m²', '').strip()
+            item_dictionary['num_of_rooms'] = item_html.find('td', {'class' : 'list-RoomNum'}).text.strip()
+            item_dictionary["overall_size"] = item_html.find('td', {'class' : 'list-AreaOverall'}).text.strip()
+            item_dictionary['floor'] = item_html.find('td', {'class' : 'list-Floors'}).text.replace('/', 'out of').strip()
+
+            item_list.append(item_dictionary)
+
+    next_page_url = driver.find_elements(By.CSS_SELECTOR, ".pagination > a")
+
+    if next_page_url[-1].get_attribute('class') != "page-bt-disabled":
+        #print(next_page_url[-1].get_attribute('href'))
+        time.sleep(3)
+        getPageData(next_page_url[-1].get_attribute('href'))
+    
+    driver.quit()
+
+
+
+getPageData("https://www.aruodas.lt/butai/vilniuje/?FRoomNumMin=1&FRoomNumMax=2&FHouseState=noteq&FOrder=Price")
+
+#print(item_list)
+
+with open('Data-'+currentTime.strftime("%m-%d-%Y_%H-%M")+'.csv', 'w', encoding="utf-16", newline='') as csvfile:
+    writer = csv.writer(csvfile)
+    writer.writerow(['Name', 'Image', 'Url', 'Price', 'Price For Sqm', 'Number of rooms', 'Size', 'Floor'])
+    for data in item_list:
+        writer.writerow([data['name'], data['image'], data['url'], data['price'], data['price_for_sqm'], data['num_of_rooms'], data['overall_size'], data['floor']])
